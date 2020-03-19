887586db2df4bebc9f3ba4ae55d887851cc4f128
ULiYJ <uliyanjun@126.com>
2020-03-19 20:43:33 +0800

    更新

--- src/main/java/com/mashibing/juc/c_020/T07_TestCyclicBarrier.java
+++ src/main/java/com/mashibing/juc/c_020/T07_TestCyclicBarrier.java
@@ -3,11 +3,14 @@ package com.mashibing.juc.c_020;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
+/**
+ * 每次阻塞线程达到 parties 时，执行目标方法
+ */
 public class T07_TestCyclicBarrier {
     public static void main(String[] args) {
         //CyclicBarrier barrier = new CyclicBarrier(20);
 
-        CyclicBarrier barrier = new CyclicBarrier(20, () -> System.out.println("满人"));
+        CyclicBarrier barrier = new CyclicBarrier(100, () -> System.out.println("满人"));
 
         /*CyclicBarrier barrier = new CyclicBarrier(20, new Runnable() {
             @Override
